caa54b0b96bc64521f0a4e89946aa7ea5a347f71
Nikita-Sychev <komap3965@icloud.com>
2023-05-24 11:57:06 +0300

    Ошибки исправлены

--- src/Main.java
+++ src/Main.java
@@ -5,36 +5,41 @@ public class Main {
         task2();
         task3();
     }
-    public static void isItLeap(int year) {
+
+    public static void checkYearOnLeap(int year) {
         if ((year % 100 != 0 && year % 4 == 0) || (year % 400 == 0)) {
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
         }
     }
+
     public static void task1() {
         System.out.println("Задача №1");
         int someYear = 2024;
-        isItLeap(someYear);
+        checkYearOnLeap(someYear);
     }
-    public static void recommendationVersion(int clientOS, int year) {
-        if (clientOS == 0 && year >= 2023) {
+
+    public static void recomnendVersion(int clientOS, int year) {
+        if (clientOS == 0 && year >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else if (clientOS == 0) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOS == 1 && year < 2024) {
+        } else if (clientOS == 1 && year < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
+
     public static void task2() {
         System.out.println("Задача 2");
         int os = 0;
-        int clientDeviceYear = 2024;
-        recommendationVersion(os, clientDeviceYear);
+        int clientDeviceYear = 2014;
+        recomnendVersion(os, clientDeviceYear);
     }
-    public static int countingDays(int distance) {
+
+    public static int countDays(int distance) {
         int day = 0;
         if (distance <= 20) {
             day++;
@@ -45,11 +50,12 @@ public class Main {
         }
         return day;
     }
+
     public static void task3() {
         System.out.println("Задача 3");
         int deliveryDistance = 30;
-        if (countingDays(deliveryDistance) != 0) {
-            System.out.println("Потребуется дней для доставки: " + countingDays(deliveryDistance));
+        if (countDays(deliveryDistance) != 0) {
+            System.out.println("Потребуется дней для доставки: " + countDays(deliveryDistance));
         } else {
             System.out.println("Доставки нет");
         }
